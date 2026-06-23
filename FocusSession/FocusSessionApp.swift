287a08e52fa67a37c24befee6c461283f652ea42
Jatin Foujdar <jatin321001@gmail.com>
2026-06-23 14:26:28 +0530

    Initial Commit

---
+++ FocusSession/FocusSessionApp.swift
@@ -0,0 +1,17 @@
+//
+//  FocusSessionApp.swift
+//  FocusSession
+//
+//  Created by jatin foujdar on 23/06/26.
+//
+
+import SwiftUI
+
+@main
+struct FocusSessionApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
